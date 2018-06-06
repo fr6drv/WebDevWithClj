ba01916ebecf3ec7b872e584894ae443a8580501
Francis Derive <francisderive@mac.com>
2018-06-06 15:36:00 +0200

    after deleting guestbook_test.db.mv.db file and "lein test" again

--- guestbook/test/clj/guestbook/test/db/core.clj
+++ guestbook/test/clj/guestbook/test/db/core.clj
@@ -9,28 +9,28 @@
 (use-fixtures
   :once
   (fn [f]
-    (mount/start
-      #'guestbook.config/env
-      #'guestbook.db.core/*db*)
+    (mount/start #'guestbook.config/env #'guestbook.db.core/*db*)
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
-(deftest test-users
+
+           
+(deftest test-messages
   (jdbc/with-db-transaction [t-conn *db*]
-    (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+        (jdbc/db-set-rollback-only! t-conn)
+        (let [timestamp (java.util.Date.)]
+            (is (= 1 (db/save-message!
+                        t-conn 
+                        {:name "Bob-test"
+                         :message "Hello Bob-test-message" 
+                         :timestamp timestamp
+                        } 
+                        {:connection t-conn})))
+            (is (= {:name "Bob-test"
+                    :message  "Hello Bob-test-message" 
+                    :timestamp timestamp}
+                    (->  (db/get-messages t-conn {})
+                        (first)
+                        (select-keys [:name :message :timestamp])))))))
+           
+           
